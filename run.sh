1ce19526a4aec8e60627e7079f016afd09a2c5b6
Ram <9160496+0-hero@users.noreply.github.com>
2022-11-29 18:20:49 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export SHELL=/bin/bash
-jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
+gc-reset && PIP_DISABLE_PIP_VERSION_CHECK=1 CACHE_DIR=/tmp DATASET_DIR=/graphcore jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
